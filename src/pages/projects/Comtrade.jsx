436f4a80e9f87bf9e0f6414104e30a8330180930
Joshua Prettyman <j.prettyman@icloud.com>
2025-06-12 12:56:31 +0200

    Implemented ContentBlock component on page

--- src/pages/projects/Comtrade.jsx
+++ src/pages/projects/Comtrade.jsx
@@ -1,35 +1,13 @@
 import React from 'react';
 import Layout from '../../components/Layout';
+import ContentBlock from '../../components/ContentBlock';
 import { Wine } from 'lucide-react';
 import { LiaGithub } from "react-icons/lia";
 
 const ComtradeProject = () => {
   return (
     <Layout>
-      <div className="max-w-3xl mx-auto py-6 sm:px-6 lg:px-8">
-
-        <div className="bg-gray-200/65 dark:bg-gray-800 rounded-lg px-8 py-6 mb-4">
-
-          <div className="flex items-center text-gray-900 dark:text-white mb-4">
-
-            <Wine className="h-8 w-8 mr-2" />
-            <h1 className="text-4xl font-bold">
-              Comtrade Data Visualisation
-            </h1>
-            <div className="font-bold flex items-center ml-auto">
-            <a 
-                href="https://github.com/DrPrettyman/comtrade" 
-                target="_blank" 
-                rel="noopener noreferrer" 
-                className="relative group text-gray-600 dark:text-gray-100 hover:text-gray-900 dark:hover:text-white transition-colors"
-              >
-                <LiaGithub className="h-10 w-10" />
-                <span className="absolute top-1/2 -left-28 -translate-y-1/2 hidden group-hover:block bg-gray-800 dark:bg-gray-600 text-white text-xs px-2 py-1 rounded whitespace-nowrap">View on GitHub</span>
-              </a>
-
-            </div>
-
-          </div>
+      <ContentBlock title="Wine Trade Data" icon={Wine} githubUrl="https://github.com/DrPrettyman/comtrade">
           
           <div className="text-gray-700 dark:text-white">
 
@@ -100,7 +78,7 @@ const ComtradeProject = () => {
             <h3 className="text-lg mb-4 font-bold">Plotly</h3>
 
             <p className="mb-4 text-justify">
-              Plotly
+              So 
             </p>
 
             <div className="my-6 bg-white rounded-lg">
@@ -160,8 +138,7 @@ const ComtradeProject = () => {
             </p>
 
           </div>
-        </div>
-      </div>
+      </ContentBlock>
     </Layout>
   )
 }

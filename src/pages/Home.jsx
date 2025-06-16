ca373d389eb119a40d401a8e34ff87d8a41d6c51
Joshua Prettyman <j.prettyman@icloud.com>
2025-06-16 17:14:42 +0200

    text formatting on homepage. Justified text, better margins and spacing.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,14 +28,16 @@ const Home = () => {
                   {' '} my name is Joshua.
                 </span>
               </div>
-              <p className="mb-8">
-                I made this website as an online résumé to showcase my work experience, skills, 
-                and projects. But then I just started having fun with it, it's really giving me nostalgia for MySpace. 
-              </p>
-              <p className="mb-8">
-                I am a data scientist with software development experience and a Ph.D. in mathematics. 
-                Please have a look around and feel free to reach out to me if you're interested in working together.
-              </p>
+              <div className='text-justify'>
+                <p className="mb-8">
+                  I made this website as an online résumé to showcase my work experience, skills, 
+                  and projects. But then I just started having fun with it, it's really giving me nostalgia for MySpace. 
+                </p>
+                <p>
+                  I am a data scientist with software development experience and a Ph.D. in mathematics. 
+                  Please have a look around and feel free to reach out to me if you're interested in working together.
+                </p>
+              </div>
             </div>
           </div>
           
@@ -48,7 +50,7 @@ const Home = () => {
             <div className="text-lg font-bold text-gray-600 dark:text-white mb-4 flex items-center">
               <Briefcase className="h-6 w-6 mr-2" /> Professional Experience
             </div>
-            <div className="text-md text-gray-600 dark:text-white">
+            <div className="text-md text-gray-600 dark:text-white text-justify">
               <p className='mb-2'>
                 In my most recent role at Blink SEO I built the company's internal software from scratch, increasing productivity by 20× by automating all data processing and generating data-lead recommendations through machine learning. 
                 This software improved the SEO process so much that we marketed it as a SaaS app to other agencies: <i>Macaroni Software</i>.
@@ -63,7 +65,7 @@ const Home = () => {
             <div className="text-lg font-bold text-gray-600 dark:text-white mb-4 flex items-center">
               <GraduationCap className="h-6 w-6 mr-2" /> Academic Background
             </div>
-            <div className="text-md text-gray-600 dark:text-white">
+            <div className="text-md text-gray-600 dark:text-white text-justify">
               <p className='mb-2'>
                 My Ph.D. focused on predicting tipping points in dynamical systems, with applications to geophysical time series data. I published three papers in respected journals and presented my research at international conferences.
               </p>
@@ -77,9 +79,10 @@ const Home = () => {
             <div className="text-lg font-bold text-gray-600 dark:text-white mb-4 flex items-center">
               <FileTerminal className="h-6 w-6 mr-2" />Tech Stack
             </div>
-            <div className="text-md text-gray-600 dark:text-white">
+            <div className="text-md text-gray-600 dark:text-white text-justify">
               <p className='mb-2'>
-                I work in Python, utilising Pandas, ScikitLearn, PyTorch and TensorFlow for data mining and machine learning tasks; and Plotly, Seaborn and Matplotlib for visualisation.
+                I work in Python, utilising Pandas, ScikitLearn, PyTorch and TensorFlow for data mining and machine learning tasks; Plotly, Seaborn and Matplotlib for visualisation; and various libraries for NLP.
+                I also use Python for web-scraping, interacting with REST and GraphQL APIs, and working through LeetCode problems.
               </p>
               <p className='mb-2'>
                 I also have extensive experience working in SQL, both for database management and data cleaning and manipulation, and have created and maintained BigQuery databases on GCP. 
@@ -96,13 +99,16 @@ const Home = () => {
             <div className="text-lg font-bold text-gray-600 dark:text-white mb-4 flex items-center">
               <Smile className="h-6 w-6 mr-2" />Bio
             </div>
-            <div className="text-md text-gray-600 dark:text-white">
+            <div className="text-md text-gray-600 dark:text-white text-justify">
               <p className='mb-2'>
-                I live in the Málaga region of Spain and spend most of my time writing code, doing DIY projects on our weird old casita, cooking vegan meals and drinking Spanish wine. 
+                I live in the Málaga region of Spain and spend most of my time writing code, doing DIY projects on our casita, cooking vegan meals and drinking Spanish wine. 
+              </p>
+              <p className='mb-2'>
+                On the weekend I'll generally be spending time with my family either at a museum in Málaga, on the beach at Torremolinos, or paddle-boarding up at Embalse del Guadalhorce. 
+                We might also head off in our campervan (Uncle Vanya) to explore Andalucía and beyond. 
               </p>
               <p>
-                On the weekend I'll generally be spending time with my family either at a museum in Málaga, on the beach at Torremolinos or paddle-boarding up at Embalse del Guadalhorce. 
-                We might also head off in our campervan to explore Andalucía and beyond. 
+                Before settling in Spain I travelled the world with my family in the campervan whilst working remotely.
               </p>
             </div>
           </div>

505da143220e11adf932aa2d10e760239a6d0837
Joshua Prettyman <j.prettyman@icloud.com>
2025-06-16 14:20:36 +0200

    Made contentblock bg slightly trasparant in dark mode

--- src/components/ContentBlock.jsx
+++ src/components/ContentBlock.jsx
@@ -4,7 +4,7 @@ import { DiGithubFull } from "react-icons/di";
 const ContentBlock = ({ title, icon: Icon, children, maxWidth = "7xl", githubUrl }) => {
   return (
     <div className={`max-w-${maxWidth} mx-auto py-6 sm:px-6 lg:px-8`}>
-      <div className="bg-gray-200/65 dark:bg-gray-800 rounded-lg px-8 py-6 mb-4 mx-2 sm:mx-0">
+      <div className="bg-gray-200/65 dark:bg-gray-800/85 rounded-lg px-8 py-6 mb-4 mx-2 sm:mx-0">
         <div className="text-gray-900 dark:text-white mb-6">
           {/* Mobile layout */}
           <div className="sm:hidden">

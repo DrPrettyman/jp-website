af300f85296024a9935e1fd2fc3e1800e5a62b64
Joshua Prettyman <j.prettyman@icloud.com>
2025-06-29 15:05:45 +0200

    New TechSkills section on /professional page

---
+++ src/components/TechSkill.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const renderText = (items, level = 0) => {
+  const listClassName = level === 0 ? "list-disc list-outside ml-5" : "list-['–'] list-outside ml-3";
+  const listItemClassName = level === 0 ? "mb-2 pl-2" : "mb-1 pl-2";
+
+  return (
+    <ul className={listClassName}>
+      {items.map((item, index) => (
+        <li key={index} className={listItemClassName}>
+          <span dangerouslySetInnerHTML={{ __html: item.content }}></span>
+          {item.children && renderText(item.children.map(child => ({ content: child })), level + 1)} 
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const TechSkill = ({ title, icon, techIcons, firstParagraph, text }) => {
+  return (
+    <div className="bg-blue-50 dark:bg-gray-600 shadow rounded-lg px-8 py-6 mb-4 mx-2 sm:mx-0">
+      <div className="text-lg font-bold text-gray-600 dark:text-white mb-4 flex items-center justify-between">
+        <div className="flex items-center">
+          {icon}
+          {title}
+        </div>
+        <div className="flex items-center space-x-2">
+          {techIcons.map((icon, index) => (
+            <img key={index} src={`/tech_icons/${icon}.svg`} alt={`${icon} Logo`} className="h-6 w-6" />
+          ))}
+        </div>
+      </div>
+      <div className="text-md text-gray-600 dark:text-white text-justify">
+        <p className='mb-2' dangerouslySetInnerHTML={{ __html: firstParagraph }}></p>
+        {renderText(text)}
+      </div>
+    </div>
+  );
+};
+
+export default TechSkill;
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,14 +1,36 @@
-import React, { useEffect } from 'react'
-import Layout from '../components/Layout'
-import CVEntry from '../components/CVEntry'
-import ContentBlock from '../components/ContentBlock'
+import React, { useEffect } from 'react';
+import Layout from '../components/Layout';
+import CVEntry from '../components/CVEntry';
+import ContentBlock from '../components/ContentBlock';
+import TechSkill from '../components/TechSkill';
 import professionalData from '../assets/cv-professional/manifest.json';
+import techSkillsData from '../assets/techSkills.json';
 import { Briefcase, FileTerminal } from 'lucide-react';
-import { VscGraphScatter } from "react-icons/vsc";
-import { PiGitPullRequest } from "react-icons/pi";
+import { PiGitPullRequest, PiPresentationChart } from "react-icons/pi";
+import { BsDatabaseCheck } from "react-icons/bs";
+import { TbChartScatter3D } from "react-icons/tb";
+
+const iconComponents = {
+  TbChartScatter3D: <TbChartScatter3D className="h-6 w-6 mr-2" />,
+  BsDatabaseCheck: <BsDatabaseCheck className="h-6 w-6 mr-2" />,
+  PiGitPullRequest: <PiGitPullRequest className="h-6 w-6 mr-2" />,
+  PiPresentationChart: <PiPresentationChart className="h-6 w-6 mr-2" />,
+};
 
 const Work = () => {
   useEffect(() => {
+    // Handle deep linking
+    const { hash } = window.location;
+    if (hash) {
+      const id = hash.substring(1);
+      setTimeout(() => {
+        const element = document.getElementById(id);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+      }, 100);
+    }
+
     // Check if we should open any modal
     const searchParams = new URLSearchParams(window.location.search);
     const openModal = searchParams.get('open');
@@ -22,12 +44,16 @@ const Work = () => {
   return (
     <Layout>
       {/* Experience */}
-      <ContentBlock title="Professional Experience" icon={Briefcase}>
+      <ContentBlock title="Professional Experience" icon={Briefcase} id="work-experience">
 
           {/* Intro text */}
           <p className="text-justify text-gray-700 dark:text-white mb-4">
-            My Mathematics Ph.D. transitioned me into a career in Data Science:
-            I'm good with statistics and problem solving, and enjoy implementing algorithms. 
+            My Ph.D. focussed on predicting tipping points in dynamical systems using multi-dimensional time series data, which I thought 
+            to be a great jumping-off point for a career in Data Science:
+            I'm good with statistics, problem solving, implementing algorithms and working with data. 
+          </p>
+
+          <p className="text-justify text-gray-700 dark:text-white mb-4">
             I work in mainly in Python and SQL, but I have experience with creating and maintaining databases, 
             dashboards and full-stack software projects. I have implemented and used a variety of 
             Machine Learning algorithms. 
@@ -56,68 +82,32 @@ const Work = () => {
             ))}
           </div>
 
+      
       </ContentBlock>
 
       {/* Tech Stack */}
-      <ContentBlock title="Technologies" icon={FileTerminal}>
+      <ContentBlock title="Technologies and Skills" icon={FileTerminal} id="tech-stack">
           {/* Intro text */}
           <div className="text-justify text-gray-700 dark:text-white mb-8">
               <p>
                 I work mainly in Python, utilising various packages for data mining, machine learning, visualisation, web-scraping, and most other things. I also have extensive experience working with SQL, JavaScript and a host of other technologies.
               </p>
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-
-            <div className="bg-gray-300/70 dark:bg-gray-600 rounded-lg px-8 py-6 mb-4 mx-2 sm:mx-0">
-              <div className="text-lg font-bold text-gray-600 dark:text-white mb-4 flex items-center">
-                <VscGraphScatter className="h-6 w-6 mr-2" /> Data Science
-              </div>
-              <div className="text-md text-gray-600 dark:text-white text-justify">
-                <p className='mb-2'>
-                  I've spent a lot of time working with ScikitLearn, PyTorch and TensorFlow for machine learning tasks, and using NLP for working with textual data.
-                </p>
-                <p className='mb-2'>
-                  NLP and DBSCAN clustering on Search Console data to provide recommendations for 
-                  primary keywords. This cut down a days-long spreadsheet-scrolling task to a few minutes. 
-                </p>
-                <p className='mb-2'>
-                  A combination of classification and clustering to recommend new category pages for D2C brands with a focus on 
-                  optimising click-through rate and capturing niche keywords whilst reducing cannibalisation.
-                </p>
-                <p>
-                  Implemented an LLM intergration to provide highly personalised recomendations for on-page copy. This allowed new pages to be launched in minutes rather than days. 
-                </p>
-              </div>
-            </div>
-
-            <div className="bg-gray-300/70 dark:bg-gray-600 rounded-lg px-8 py-6 mb-4 mx-2 sm:mx-0">
-              <div className="text-lg font-bold text-gray-600 dark:text-white mb-4 flex items-center">
-                <PiGitPullRequest className="h-6 w-6 mr-2" /> Software Development 
-              </div>
-              <div className="text-md text-gray-600 dark:text-white text-justify">
-                <p className='mb-2'>
-                  Built the <i>Macaroni Software</i> backend in Python running on GCP Compute Engine. This connected to client data sources via APIs, handled Data Cleaning and ML tasks, and integrated with BigQuery.
-                </p>
-                <p className='mb-2'>
-                  I wrote wrappers (Python) for various APIs and data processing steps (Pandas). This saved the team from the manual csv-export/excel-import repetative nightmare and freed up countless hours.
-                </p>
-                <p className='mb-2'>
-                  Set up Slack notifications from the backend system to alert devs (me) of tech problems and agency staff of clients' statuses.
-                </p>
-                <p className='mb-2'>
-                  Created a job-queue system (PostgreSQL) for running back-end processes on demand and on schedules.
-                </p>
-                <p className='mb-2'>
-                  Created numerous apps and dashboards in Retool connected to data in BigQuery and the backend via the job-queue. 
-                  Apps drastically increased productivity by automating manual data-engineering tasks. Dashboards replaced PDF reports, saving account managers several hours each month per client.
-                </p>
-              </div>
-            </div>
-            
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+            {techSkillsData.map((skill, index) => (
+              <TechSkill
+                key={index}
+                title={skill.title}
+                icon={iconComponents[skill.icon]}
+                techIcons={skill.techIcons}
+                firstParagraph={skill.firstParagraph}
+                text={skill.text}
+              />
+            ))}
           </div>
       </ContentBlock>
     </Layout>
   )
 }
 
-export default Work
+export default Work;

9773f7a6c5b869b99b670d06bc099480185523d5
Joshua Prettyman <j.prettyman@icloud.com>
2025-06-30 13:41:04 +0200

    refactored python files into a module

---
+++ markdown/generate_cv.py
@@ -0,0 +1,122 @@
+import os
+import re
+import json
+import sys
+from bs4 import BeautifulSoup
+
+from parse import html_to_markdown
+
+
+class CV:
+    def __init__(self, assets_path: str):
+        self.p = assets_path
+        
+    def header(self):
+        h = """# Joshua Prettyman, Ph.D.\n\n"""
+        h += "Data Scientist, Mathematics Ph.D., Python Developer\n\n"
+        contacts = [
+            "[joshua@prettyman.me](mailto:joshua@prettyman.me)", 
+            "[linkedin.com/in/joshuaprettyman](https://www.linkedin.com/in/joshuaprettyman/)", 
+            "[joshua.prettyman.me](https://joshua.prettyman.me)"
+        ]
+        h += " | ".join(contacts)
+        h += "\n\n"
+        return h
+        
+    def professional_experience(self) -> str:
+        section = "# Professional Experience\n\n"
+        
+        p = os.path.join(self.p, "cv-professional")
+        
+        with open(os.path.join(p, "intro-experience.html"), "r") as _f:
+            _md = html_to_markdown(_f.read())
+        
+        section += _md
+        section += "\n\n"
+            
+        with open(os.path.join(p, "manifest.json"), "r") as f:
+            manifest = json.load(f)
+
+        for r in manifest:
+            s = f"## {r['title']}\n\n"
+            s += f"**{r['company']} | {r['duration']}**\n\n"
+            with open(os.path.join(p, r['entryId']+".shtml"), "r") as _f:
+                _md = html_to_markdown(_f.read())
+            s += _md
+            s += "\n\n"
+            section += s
+        
+        return section
+    
+    def _parse_tech_skill(self, record: dict):
+        s = f"## {record['title']}\n\n"
+        s += record['firstParagraph'] + "\n"
+        for _d in record['text']:
+            s += f"- {_d['content']}\n"
+            if children := _d.get('children'):
+                for c in children:
+                    s += f"\t- {c}\n"
+        return s
+    
+    def tech_skills(self):
+        section = "# Technologies and Skills\n\n"
+        
+        intro_path = os.path.join(self.p, "cv-professional", "intro-tech-stack.html")
+        with open(intro_path, "r") as _f:
+            _md = html_to_markdown(_f.read())
+        section += _md
+        section += "\n\n"      
+        
+        with open(os.path.join(self.p, "techSkills.json"), "r") as _f:
+            info = json.load(_f)
+            
+        for _record in info:
+            section += self._parse_tech_skill(_record) + "\n"
+            
+        return section
+    
+    def academic_experience(self):
+        section = "# Academic Background\n\n"
+        
+        p = os.path.join(self.p, "cv-academic")
+        
+        with open(os.path.join(p, "intro-academic.html"), "r") as _f:
+            _md = html_to_markdown(_f.read())
+        
+        section += _md
+        section += "\n\n"
+            
+        with open(os.path.join(p, "manifest.json"), "r") as f:
+            manifest = json.load(f)
+
+        for r in manifest:
+            s = f"## {r['title']}\n\n"
+            s += f"**{r['company']}**"
+            if 'grade' in r:
+                s += f" ({r['grade']})"
+            s += "\n\n"
+            
+            with open(os.path.join(p, r['entryId']+".shtml"), "r") as _f:
+                _md = html_to_markdown(_f.read())
+            s += _md
+            s += "\n\n"
+            section += s
+        
+        return section
+    
+    def to_md(self, file: str = None) -> str | None:
+        cv = self.header()
+        cv += self.professional_experience()
+        cv += self.tech_skills()
+        cv += self.academic_experience()
+        
+        if file:
+            with open(file, "w") as _f:
+                _f.write(cv)
+        else:
+            return cv
+
+
+if __name__ == "__main__":
+    cv = CV("../src/assets")
+    cv.to_md("cv.md")
